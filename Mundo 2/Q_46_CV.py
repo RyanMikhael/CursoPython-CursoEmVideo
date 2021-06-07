d21584554948b55993b4453c5b4f90eabd4ee569
RyanMikhael <62043977+RyanMikhael@users.noreply.github.com>
2021-06-07 18:17:21 -0300

    Delete Mundo 2 directory

--- Mundo 2/Q_46_CV.py
+++
@@ -1,9 +0,0 @@
-#Questão 46 do curso em vídeo
-from time import sleep
-
-'''Programa que faz uma contagem regressiva'''
-
-for i in range(10, -1, -1):
-    print(i)
-    sleep(1)
-print('BUUUUUMMMM!!!')
--- Mundo 2/Q_47_CV.py
+++
@@ -1,8 +0,0 @@
-#Questão 47 do curso em vídeo
-
-'''Programa que mostra todos os numeros pares entre 1 e 50'''
-
-for i in range(1, 51):
-    if i % 2 == 0:
-        print(i, end= ' ')
-print('FIM!')
